5d29f8bbd99df627ffad471e941f3d1e5a7f46d3
Louis Chavey <louis@chavey.org>
2024-02-16 16:28:59 -0600

    feature 2 working

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from distutils.log import debug 
 from fileinput import filename 
 from flask import *
+import shutil
+import os
+import subprocess
 app = Flask(__name__) 
+
+upload_folder = 'uploads'
+app.config['UPLOAD_FOLDER'] = upload_folder  # set configuration var to make the value of UPLOAD_FOLDER available thruout the Flask application
 
 @app.route('/') 
 def main(): 
 	return render_template("index.html") 
 
-@app.route('/success', methods = ['POST']) 
+@app.route('/upload', methods = ['POST']) 
 def success(): 
-	if request.method == 'POST': 
-		f = request.files['file'] 
-		f.save(f.filename) 
-		return render_template("ACK.html", name = f.filename) 
+	f = request.files['file'] 
+	if f:
+		if f.filename == '':
+			f.filename = "Untitled"
+		f.save(f.filename)
+		file_copy = f"{os.path.splitext(f.filename)[0]}_copy{os.path.splitext(f.filename)[1]}"
+		shutil.copy2(os.path.realpath(f.filename), os.path.realpath(file_copy))
+		os.remove(f.filename)
+		return render_template("ACK_reupload.html", name = f.filename)
+
+@app.route('/run', methods = ['POST'])
+def run():
+	script = request.form.get('script_filename')
+	if os.path.isfile(script):
+		#return script
+		# path = 'run.py'
+		# spawn subprocess w/ default input of physics_comments
+		#try:
+		subprocess.run(["python", script], shell=True, check=True)
+			# output = subprocess.check_output(['python', 'run.py'], stderr=subprocess.STDOUT, universal_newlines=True)
+			# return jsonify({"output": output}), 200
+		return render_template("ACK_run.html", name = script)
+		#except subprocess.CalledProcessError as e:
+		# 	return jsonify({'error':f'Script execution failed: {e.output}'}, 500)	
+		# create a new output file
 
 if __name__ == '__main__': 
 	app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 TO-DO:
-* Add in continuous loop of file uploading
-* Make a copy of the uploaded file so that we don't edit the local copy
+* SUBGOAL 1: Get the basic functionality on a FLASK server -- 2/10-2/16
+    * Feature 1: Set up files and scripts in server - DONE 2/15
+        * Add in continuous loop of file uploading - DONE 2/15
+        * Make a copy of the uploaded file so that we don't edit the local copy - DONE 2/15
+    * Feature 2: Run scripts on files and store output remotely
+        * Choose program to run thru typing in file name - DONE 2/16
+        * Edit all existing .ipynb files:
+            * They're actual scripts
+            * We can enter parameters
+            * They create a graph output
+        * Display which scripts are currently in the directory
+* SUBGOAL 2: Make a nicer frontend (move to REACT/tailwind) and make any frontend-enabled functionality improvements -- 2/17-2/23
+    * Feature 2: Run script from a dropdown
+* SUBGOAL 3: Run this on Prof. Sherin's server (and/or QUEST?) -- 2/24-3/1
+* SUBGOAL 4: Finishing touch features + improve existing ones
+    * Feature 3: Authentication system (especially if run on QUEST)
+        * Make a family and friends version of the site w/ static graphs
+        * Security considerations?
+    * Feature 4: Progress bar on jobs
+    * Feature 1: Drag and drop
+    
