ac7e195675bbec5a53839a44c9dae8d85454389f
Louis Chavey <louis@chavey.org>
2024-03-04 18:23:28 -0600

    Revert ".py parse works"
    This reverts commit 7ed78db23d72ca86278a2fe4898b04effe32c3cf.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from flask import *
 import shutil
 import os
 import subprocess
-import ast 
-
 app = Flask(__name__) 
 
 upload_folder = 'uploads'
@@ -15,10 +13,6 @@ app.config['UPLOAD_FOLDER'] = upload_folder  # set configuration var to make the
 def main(): 
 	return render_template("index.html") 
 
-
-# /upload 
-# This is a function that's called whenever a script file is uploaded 
-#
 @app.route('/upload', methods = ['POST']) 
 def success(): 
 	f = request.files['file'] 
@@ -31,46 +25,12 @@ def success():
 		os.remove(f.filename)
 		return render_template("ACK_run.html", name = f.filename)
 
-
-# parse()
-# This is a function that checks for how many inputs are needed from a script. 
-# This should send a JSON to the frontend to add more input boxes. 
-# Searches for a variable with the name `argNum`, example shown below 
-# ```
-# argNum = 0
-# ```
-@app.route('/parse', methods = ['POST'])
-def parse(script_name: str) -> int:   
-	with open(script_name, "r") as f:  
-		code = f.readlines()
-		first_line = code[0].split()
-		argNum = 0 
-		try: 
-			if ("argNum" in first_line): 
-				argNum = first_line[-1]
-
-		except: 
-			print("Couldn't find argNum :(")
-
-	return argNum
-
-
-
-
-# /run 
-# After importing a script, run the script with the suppliedi nputs 
 @app.route('/run', methods = ['POST'])
 def run():
 	script = request.form.get('script_filename')
 	if os.path.isfile(script):
-
-		argNum = parse(script)
-		print(argNum)
 		# with open("Physics_submissions.zst", 'r') as f:
 			# try:
-
-		# TODO: Work on making this more dynamic and responsive 
-		# for taking in inputs 
 		subprocess.run(["python", script], input='physics_submissions.csv\nphysics_comments.csv', shell=True, text=True, check=True)
 		return render_template("ACK_run.html", name = script)
 		# except subprocess.CalledProcessError as e:
